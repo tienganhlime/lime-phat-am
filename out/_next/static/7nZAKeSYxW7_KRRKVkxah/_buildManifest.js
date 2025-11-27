edc7223655f97a1ee71e223b26572764d83b091f
tienganhlime <linhtrinh.ms@gmail.com>
2025-11-27 03:14:40 +0000

    deploy static site

---
+++ out/_next/static/7nZAKeSYxW7_KRRKVkxah/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/lime-phat-am//_next/:path+",
+        "destination": "/lime-phat-am/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
